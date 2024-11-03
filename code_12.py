5270349257919081d575d53dbbfeae65ad96f4a7
alemunoz10 <adromero6@miners.utep.edu>
2024-11-03 11:34:25 -0700

    I got the first one down on being able to print out the posiiton

--- code_12.py
+++ code_12.py
@@ -1,8 +1,33 @@
 def get_fibonacci_number(position):
-    pass #Remove this line and insert your code here. Do not forget this function implements recursion.
+    if position == 0:
+        return 0
+    if position == 1:
+        return 1
+    
+    return get_fibonacci_number(position-2)+get_fibonacci_number(position-1)
 
-def get_fibonacci_number_sequence(number):
-    pass #Remove this line and insert your code here. Do not forget to use get_fibonacci_number to create your list of numbers.
+
+def get_fibonacci_number_sequence(n):
+    if n == 0:
+        return [0]
+    if n == 1:
+        return [0,1]
+    
+    previous_x = 0
+    previous_y = 1
+
+    number_sequence = [0,1]
+
+    for i in range(2, n+1):
+        current = previous_x + previous_y
+        previous_x = previous_y
+        previous_y = current
+
+        number_sequence.append(current)
+
+    return number_sequence
 
 if __name__ == "__main__":
-    pass #Remove this line and insert your code to test your Fibonacci function here
+   position = 3
+   fib_num = get_fibonacci_number(position)
+   print(f"The fibonacci number in position {position} is {fib_num}")
